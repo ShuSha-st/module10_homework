410d0d3f9bfe83bb6b62740c970108a2ac1506c7
ShuSha-st <Bhstella@yandex.ru>
2020-10-03 10:59:42 +0400

    first commit

---
+++ Homework_10_2.js
@@ -0,0 +1,5 @@
+var rateDoll = prompt('введите курс доллара');
+var quotDoll = prompt('введите количество долларов для обмена');
+rateRub = rateDoll * quotDoll;
+console.log(rateRub);
+alert('Сумма в рублях для конверсии: ' + rateRub)
